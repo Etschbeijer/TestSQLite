a23742d78d60b6aa6dffb2cbcc9529b6089ddfea
patrick blume <PatrickBlume@gmx.de>
2017-12-22 11:44:36 +0100

    Projektdateien hinzufügen.

---
+++ TestSQLite/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace TestSQLite.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Allgemeine Informationen über eine Assembly werden über die folgende 
+// Attributgruppe gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[<assembly: AssemblyTitle("TestSQLite")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("TestSQLite")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Durch Festlegen von ComVisible auf 'false' werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[<assembly: ComVisible(false)>]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[<assembly: Guid("c634c2e5-b85b-480a-8f44-1ef5def6b5f6")>]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+// 
+//       Hauptversion
+//       Nebenversion
+//       Buildnummer
+//       Revision
+// 
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [<Assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ TestSQLite/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace TestSQLite
+
+type Class1() = 
+    member this.X = "F#"
