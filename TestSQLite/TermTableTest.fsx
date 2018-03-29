2af6f86b343256dee09f80f294b2fdde4b2c5022
patrick blume <PatrickBlume@gmx.de>
2018-03-29 15:52:26 +0200

    Update

--- TestSQLite/TermTableTest.fsx
+++ TestSQLite/TermTableTest.fsx
@@ -1,4 +1,5 @@
-﻿#I @"..\packages\Microsoft.AspNet.Identity.EntityFramework.2.2.1\lib\net45"
+﻿
+#I @"..\packages\Microsoft.AspNet.Identity.EntityFramework.2.2.1\lib\net45"
 #I @"..\packages\Microsoft.AspNet.Identity.Core.2.2.1\lib\net45\"
 #r "Microsoft.AspNet.Identity.Core.dll"
 #r "Microsoft.AspNet.Identity.EntityFramework.dll"
@@ -8,11 +9,13 @@
 #r @"..\TestSQLite\bin\Debug\Microsoft.EntityFrameworkCore.Relational.dll"
 #r @"..\TestSQLite\bin\Debug\Microsoft.EntityFrameworkCore.Sqlite.dll"
 #r @"..\TestSQLite\bin\Debug\System.Data.SQLite.dll"
-//#r @"..\TestSQLite\bin\Debug\BioFSharp.dll"
-//#r @"..\TestSQLite\bin\Debug\BioFSharp.IO.dll"
+#r @"..\TestSQLite\bin\Debug\BioFSharp.dll"
+#r @"..\TestSQLite\bin\Debug\BioFSharp.IO.dll"
 //#r @"..\TestSQLite\bin\Debug\BioFSharp.Mz.dll"
 #r @"..\TestSQLite\bin\Debug\FSharp.Care.dll"
 #r @"..\TestSQLite\bin\Debug\FSharp.Care.IO.dll"
+
+let fileDir = __SOURCE_DIRECTORY__
 
 open System
 //open System.Diagnostics
@@ -21,17 +24,18 @@ open Microsoft.EntityFrameworkCore
 //open System.Linq
 //open BioFSharp.BioItem
 open System.Collections.Generic
-
-
+open FSharp.Care.IO
+open BioFSharp.IO
 ///Defining Types
 
 type [<CLIMutable>] Ontology = 
     {
     [<DatabaseGenerated(DatabaseGeneratedOption.Identity)>]
     ID             : int
-    Name           : string
+    [<Column("Name")>]
+    OntologyName   : string
     RowVersion     : DateTime
-    OntologyParams : OntologyParam
+    OntologyParams : List<OntologyParam>
     Terms          : List<Term>
     }
 
@@ -71,11 +75,13 @@ and [<CLIMutable>] SpectrumIdentificationParam =
 
 and [<CLIMutable>] Term =
     {
-    ID         : string
-    Name       : string
+    ID             : string
+    Name           : string
     //OntologyID : string
-    Ontology   : Ontology
-    RowVersion : DateTime 
+    (*[<ForeignKey("OntologyName")>]*)
+    Ontology     : Ontology
+    RowVersion     : DateTime 
+    OntologyParams : List<OntologyParam>
     }
 
 //and [<CLIMutable>] TermRelationShip =
@@ -106,6 +112,10 @@ type DBMSContext() =
     member public this.Ontology with get() = this.m_ontology
                                                 and set value = this.m_ontology <- value
 
+    [<DefaultValue>] val mutable m_ontologyParam : DbSet<OntologyParam>
+    member public this.OntologyParam with get() = this.m_ontologyParam
+                                                and set value = this.m_ontologyParam <- value
+
     [<DefaultValue>] val mutable m_spectrumIdentification : DbSet<SpectrumIdentification>
     member public this.SpectrumIdentification with get() = this.m_spectrumIdentification
                                                     and set value = this.m_spectrumIdentification <- value
@@ -126,15 +136,201 @@ type DBMSContext() =
     //member public this.TermTag with get() = this.m_termTag
     //                                    and set value = this.m_termTag <- value
 
+    override this.OnModelCreating (modelbuilder : ModelBuilder) =
+        modelbuilder.Entity<OntologyParam>()
+            .HasOne(fun field -> field.FKTerm)
+            .WithMany("OntologyParams")
+            .IsRequired(true) |> ignore
+
     override this.OnConfiguring (optionsBuilder :  DbContextOptionsBuilder) =
         let fileDir = __SOURCE_DIRECTORY__ 
-        let dbPath = fileDir + "\Ontologies_Terms\DavidsDatenbank.db"
+        let dbPath = fileDir + "\Ontologies_Terms\MSDatenbank.db"
         optionsBuilder.EnableSensitiveDataLogging() |> ignore
         optionsBuilder.UseSqlite(@"Data Source="+ dbPath) |> ignore
 
+///Define reader for OboFile
+let fromFileObo (filePath : string) =
+    FileIO.readFile filePath
+    |> Obo.parseOboTerms
 
-///Creating/Checking for Database
+let fromPsiMS =
+    fromFileObo (fileDir + "\Ontologies_Terms\Psi-MS.txt")
 
+let fromPride =
+    fromFileObo (fileDir + "\Ontologies_Terms\Pride.txt")
+
+let fromUniMod =
+    fromFileObo (fileDir + "\Ontologies_Terms\Unimod.txt")
+
+let fromUnit_Ontology =
+    fromFileObo (fileDir + "\Ontologies_Terms\Unit_Ontology.txt")
+
+///Define Types to insert into DB
+let ontologyPsiMS = 
+                {
+                 Ontology.ID = 0
+                 Ontology.OntologyName = "Psi-MS"
+                 Ontology.RowVersion = DateTime.Now.Date
+                 Ontology.OntologyParams = null
+                 Ontology.Terms = new System.Collections.Generic.List<Term>
+                    (fromPsiMS
+                    |> Seq.map (fun item -> {
+                                             Term.ID = item.Id; 
+                                             Term.Name = item.Name; 
+                                             Term.Ontology = {
+                                                              Ontology.ID = 0; 
+                                                              Ontology.OntologyName = "Psi-MS"; 
+                                                              Ontology.RowVersion = DateTime.Now.Date; 
+                                                              Ontology.OntologyParams = null; 
+                                                              Ontology.Terms = null
+                                                             }; 
+                                             Term.RowVersion = DateTime.Now.Date; 
+                                             Term.OntologyParams = null
+                                            }
+                                )
+                    )
+                }
+
+let ontologyPride = 
+                {
+                 Ontology.ID = 0
+                 Ontology.OntologyName = "Pride"
+                 Ontology.RowVersion = DateTime.Now.Date
+                 Ontology.OntologyParams = null
+                 Ontology.Terms = new System.Collections.Generic.List<Term>
+                    (fromPride
+                    |> Seq.map (fun item -> {
+                                             Term.ID = item.Id; 
+                                             Term.Name = item.Name; 
+                                             Term.Ontology = {
+                                                              Ontology.ID = 0; 
+                                                              Ontology.OntologyName = "Pride"; 
+                                                              Ontology.RowVersion = DateTime.Now.Date; 
+                                                              Ontology.OntologyParams = null; 
+                                                              Ontology.Terms = null
+                                                             }; 
+                                             Term.RowVersion = DateTime.Now.Date; 
+                                             Term.OntologyParams = null
+                                            }
+                                )
+                    )
+                }
+
+let ontologyUniMod = 
+                {
+                 Ontology.ID = 0
+                 Ontology.OntologyName = "UniMod"
+                 Ontology.RowVersion = DateTime.Now.Date
+                 Ontology.OntologyParams = null
+                 Ontology.Terms = new System.Collections.Generic.List<Term>
+                    (fromUniMod
+                    |> Seq.map (fun item -> {
+                                             Term.ID = item.Id; 
+                                             Term.Name = item.Name; 
+                                             Term.Ontology = {
+                                                              Ontology.ID = 0; 
+                                                              Ontology.OntologyName = "UniMod"; 
+                                                              Ontology.RowVersion = DateTime.Now.Date; 
+                                                              Ontology.OntologyParams = null; 
+                                                              Ontology.Terms = null
+                                                             }; 
+                                             Term.RowVersion = DateTime.Now.Date; 
+                                             Term.OntologyParams = null
+                                            }
+                                )
+                    )
+                }
+
+let ontologyUnit_Ontology = 
+                {
+                 Ontology.ID = 0
+                 Ontology.OntologyName = "Unit_Ontology"
+                 Ontology.RowVersion = DateTime.Now.Date
+                 Ontology.OntologyParams = null
+                 Ontology.Terms = new System.Collections.Generic.List<Term>
+                    (fromUnit_Ontology
+                    |> Seq.map (fun item -> {
+                                             Term.ID = item.Id; 
+                                             Term.Name = item.Name; 
+                                             Term.Ontology = {
+                                                              Ontology.ID = 0; 
+                                                              Ontology.OntologyName = "Unit_Ontology"; 
+                                                              Ontology.RowVersion = DateTime.Now.Date; 
+                                                              Ontology.OntologyParams = null; 
+                                                              Ontology.Terms = null
+                                                             }; 
+                                             Term.RowVersion = DateTime.Now.Date; 
+                                             Term.OntologyParams = null
+                                            }
+                                )
+                    )
+                }
+
+let Unitless = {
+                Ontology.ID = -1
+                Ontology.OntologyName = ""
+                Ontology.RowVersion = DateTime.Now.Date
+                Ontology.OntologyParams = null
+                Ontology.Terms = new System.Collections.Generic.List<Term>
+                    ([{
+                       Term.ID = ""; 
+                       Term.Name = ""; 
+                       Term.Ontology = {
+                                        Ontology.ID = -1; 
+                                        Ontology.OntologyName = ""; 
+                                        Ontology.RowVersion = DateTime.Now.Date; 
+                                        Ontology.OntologyParams = null; 
+                                        Ontology.Terms = null
+                                       }; 
+                       Term.RowVersion = DateTime.Now.Date; 
+                       Term.OntologyParams = null
+                   }])
+               }
+
+//let ontoTest2 = {
+//                 Ontology.ID = 0
+//                 Ontology.OntologyName = ""
+//                 Ontology.RowVersion = DateTime.Now.Date
+//                 Ontology.OntologyParams = null
+//                 Ontology.Terms = null
+//                }
+//let termTest1 = {
+//                 Term.ID = "Testi"
+//                 Term.Name = "TestOntology"
+//                 Term.RowVersion = DateTime.Now.Date
+//                 Term.Ontology = ontologyPsiMS
+//                 Term.OntologyParams = null
+//                }
+//let termTest2 = {
+//                 Term.ID = ""
+//                 Term.Name = "TestOntology"
+//                 Term.RowVersion = DateTime.Now.Date
+//                 Term.Ontology = ontoTest2
+//                 Term.OntologyParams = null
+//                }
+//let ontoParamTest = {
+//                     OntologyParam.ID = 0
+//                     OntologyParam.Value = "Zero"
+//                     OntologyParam.FKParamContainer = ontologyPsiMS
+//                     OntologyParam.FKTerm = termTest1
+//                     OntologyParam.FKUnit = termTest2
+//                     OntologyParam.RowVersion = DateTime.Now.Date
+//                    }
+
+///Creating && Checking && inserting into DB
 let initDB =
     let db = new DBMSContext()
     db.Database.EnsureCreated() |> ignore
+
+    db.Ontology.Add(ontologyPsiMS)         |> ignore
+    db.Ontology.Add(ontologyPride)         |> ignore
+    db.Ontology.Add(ontologyUniMod)        |> ignore
+    db.Ontology.Add(ontologyUnit_Ontology) |> ignore
+    db.Ontology.Add(Unitless)              |> ignore
+    db.SaveChanges()
+    
+//type Test =
+//    {Variable : int}
+
+//let x = List<Test>().FirstOrDefault()
+//if (box x = null) then None else Some(x)
